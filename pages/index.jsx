a2f037f2b98dbf2fa77529fd7da9c0ec07a95531
ren89 <ren.avellano@gmail.com>
2023-07-11 12:36:19 +0800

    login page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,104 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(8).max(50),
+});
 
 export default function Home() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
   return (
     <main className="min-h-screen flex">
       <div className="w-1/2 min-h-full relative">
         <Image src="/images/building.jpg" alt="Building" fill />
       </div>
-      <div className="w-1/2 min-h-full flex justify-center items-center">
-        <h1 className="uppercase text-emerald-500 font-black tracking-tight text-4xl">
+      <div className="w-1/2 min-h-full flex flex-col justify-center items-center">
+        <h1 className="uppercase text-emerald-500 font-black tracking-tight text-4xl mb-4">
           Buildustry
         </h1>
+        <Card>
+          <CardHeader>
+            <CardTitle className=" font-bold text-2xl">Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col gap-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Username" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button className="bg-emerald-500 w-full mt-4" type="submit">
+                  Login
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter>
+            <p>
+              Don't have an account yet?{" "}
+              <Link href="#" className="text-emerald-500 font-semibold">
+                Register Now
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
       </div>
     </main>
   );
