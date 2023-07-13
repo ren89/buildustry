d818f3438d938fc0920ffd56688b5cb48b8dce6b
swtmply <delosreyesjohnallen@gmail.com>
2023-07-13 14:04:54 +0800

    added row actions

---
+++ components/row-actions-dropdown.jsx
@@ -0,0 +1,44 @@
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
+import WorkerProfileDialog from "./worker-profile-dialog";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { DialogTrigger } from "./ui/dialog";
+
+const RowActionsDropdown = ({ worker }) => {
+  return (
+    <WorkerProfileDialog
+      worker={worker}
+      onDropdown
+      dropdownMenu={
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild className="float-right">
+            <Button variant="ghost">
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link href={`/dashboard/messages`} className="flex gap-2">
+                <span>Message</span>
+              </Link>
+            </DropdownMenuItem>
+            <DialogTrigger asChild>
+              <DropdownMenuItem>
+                <span>View Portfolio</span>
+              </DropdownMenuItem>
+            </DialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      }
+    />
+  );
+};
+
+export default RowActionsDropdown;
--- components/worker-profile-dialog.jsx
+++ components/worker-profile-dialog.jsx
@@ -11,43 +11,54 @@ import Rating from "./rating";
 import { Separator } from "./ui/separator";
 import ProjectRequestDialog from "./project-request-dialog";
 
-const WorkerProfileDialog = ({ children, worker }) => {
+export const WorkerProfileContent = ({ worker }) => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center gap-4 justify-between">
+        <div className="flex gap-4">
+          <div className="w-24 h-24 relative">
+            <Image
+              src="https://picsum.photos/500/500"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex flex-col">
+            <p className="text-xl font-bold text-emerald-500">{`${worker.firstName} ${worker.lastName}`}</p>
+            <p className="text-slate-500 text-sm">{worker.email}</p>
+            <p className="text-medium float-right">{worker.contactNumber}</p>
+            <Rating value={worker.rating} />
+          </div>
+        </div>
+        <div className="self-start">
+          <ProjectRequestDialog role={worker.role} worker={worker} />
+        </div>
+      </div>
+      <Separator />
+      <DialogTitle>Projects Done</DialogTitle>
+    </div>
+  );
+};
+
+const WorkerProfileDialog = ({
+  children,
+  worker,
+  onDropdown,
+  dropdownMenu,
+}) => {
   const [open, setOpen] = useState(false);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>{children}</DialogTrigger>
+      {onDropdown && dropdownMenu}
+
+      {!onDropdown && <DialogTrigger asChild>{children}</DialogTrigger>}
       <DialogContent className="max-w-xl">
         <DialogHeader>
           <DialogTitle>Worker Profile</DialogTitle>
         </DialogHeader>
-        <div className="space-y-4">
-          <div className="flex items-center gap-4 justify-between">
-            <div className="flex gap-4">
-              <div className="w-24 h-24 relative">
-                <Image
-                  src="https://picsum.photos/500/500"
-                  alt=""
-                  fill
-                  className="rounded-full"
-                />
-              </div>
-              <div className="flex flex-col">
-                <p className="text-xl font-bold text-emerald-500">{`${worker.firstName} ${worker.lastName}`}</p>
-                <p className="text-slate-500 text-sm">{worker.email}</p>
-                <p className="text-medium float-right">
-                  {worker.contactNumber}
-                </p>
-                <Rating value={worker.rating} />
-              </div>
-            </div>
-            <div className="self-start">
-              <ProjectRequestDialog role={worker.role} worker={worker} />
-            </div>
-          </div>
-          <Separator />
-          <DialogTitle>Projects Done</DialogTitle>
-        </div>
+        <WorkerProfileContent worker={worker} />
       </DialogContent>
     </Dialog>
   );
