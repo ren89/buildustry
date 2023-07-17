f70e59ddf5b03c1d86aa3233f081c78309c5270b
Jheremiah Figueroa <figueroa.jheremiah@gmail.com>
2023-07-17 21:59:19 +0800

    Removing Socket IO kasi sira sa NextJS 13

--- pages/api/messages/inbox.js
+++ pages/api/messages/inbox.js
@@ -1,68 +1,41 @@
 // pages/api/message/index.js
 
-import { Server } from "socket.io";
-import { createServer } from "http";
-import asyncHandler from "@/middlewares/asyncHandler";
-import authMiddleware from "@/middlewares/authMiddleware";
-import { prisma } from "@/lib/db";
-import { userHelper } from "@/lib/helper";
-
-let io;
-
-export const config = {
-  api: {
-    bodyParser: true,
-  },
-};
+import asyncHandler from '@/middlewares/asyncHandler';
+import authMiddleware from '@/middlewares/authMiddleware';
+import { prisma } from '@/lib/db';
+import { userHelper } from '@/lib/helper';
 
 const getMessages = authMiddleware(async (req, res) => {
-  // Id of current user
-  const { id: receiverId } = req.user;
-  // Fetch all messages where the receiverId and senderId match
-  const messages = await prisma.message.findMany({
-    where: {
-      receiverId,
-    },
-    include: {
-      sender: {
-        select: userHelper,
-      },
-      receiver: {
-        select: userHelper,
-      },
-    },
-    orderBy: { createdAt: "desc" },
-    distinct: ["senderId"],
-  });
-  io.emit("messageUpdate", messages);
-  res.status(200).json(messages);
+	// Id of current user
+	const { id: receiverId } = req.user;
+	// Fetch all messages where the receiverId and senderId match
+	const messages = await prisma.message.findMany({
+		where: {
+			receiverId,
+		},
+		include: {
+			sender: {
+				select: userHelper,
+			},
+			receiver: {
+				select: userHelper,
+			},
+		},
+		orderBy: { createdAt: 'desc' },
+		distinct: ['senderId'],
+	});
+	res.status(200).json(messages);
 });
 
 const handler = asyncHandler(async (req, res) => {
-  if (!res.socket.server.io) {
-    console.log("New Io server connected");
-    const httpServer = createServer(req.socket.server.app);
-    io = new Server(httpServer);
-
-    io.on("connection", (socket) => {
-      console.log(`New client connected: ${socket.id}`);
-      socket.on("disconnect", () => {
-        console.log(`Client disconnected: ${socket.id}`);
-      });
-    });
-
-    res.socket.server.io = io;
-  } else {
-    io = res.socket.server.io;
-  }
-  switch (req.method) {
-    case "GET":
-      await getMessages(req, res);
-      break;
-    default:
-      res.status(405).json({ message: "Method not allowed" });
-      break;
-  }
+	switch (req.method) {
+		case 'GET':
+			await getMessages(req, res);
+			break;
+		default:
+			res.status(405).json({ message: 'Method not allowed' });
+			break;
+	}
 });
 
 export default handler;
--- pages/api/messages/index.js
+++ pages/api/messages/index.js
@@ -1,19 +1,9 @@
 // pages/api/message/index.js
 
-import { Server } from 'socket.io';
-import { createServer } from 'http';
 import asyncHandler from '@/middlewares/asyncHandler';
 import authMiddleware from '@/middlewares/authMiddleware';
 import { prisma } from '@/lib/db';
 import { userHelper } from '@/lib/helper';
-
-let io;
-
-export const config = {
-	api: {
-		bodyParser: true,
-	},
-};
 
 const sendMessage = authMiddleware(async (req, res) => {
 	// Id of current user
@@ -38,7 +28,6 @@ const sendMessage = authMiddleware(async (req, res) => {
 		},
 	});
 
-	io.emit('newMessage', message);
 	res.status(201).json(message);
 });
 
@@ -69,27 +58,10 @@ const getMessages = authMiddleware(async (req, res) => {
 		},
 		orderBy: { createdAt: 'asc' },
 	});
-	io.emit('messageUpdate', messages);
 	res.status(200).json(messages);
 });
 
 const handler = asyncHandler(async (req, res) => {
-	if (!res.socket.server.io) {
-		console.log('New Io server connected');
-		const httpServer = createServer(req.socket.server.app);
-		io = new Server(httpServer);
-
-		io.on('connection', (socket) => {
-			console.log(`New client connected: ${socket.id}`);
-			socket.on('disconnect', () => {
-				console.log(`Client disconnected: ${socket.id}`);
-			});
-		});
-
-		res.socket.server.io = io;
-	} else {
-		io = res.socket.server.io;
-	}
 	switch (req.method) {
 		case 'POST':
 			await sendMessage(req, res);
