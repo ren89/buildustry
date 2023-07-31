622d36f43d3c606612091f76fcf51fb85b201e7b
ren89 <ren.avellano@gmail.com>
2023-07-31 21:08:31 +0800

    search

--- components/dashboard-layout.jsx
+++ components/dashboard-layout.jsx
@@ -1,5 +1,5 @@
 import { LogOut, User, UserCircle } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./navigation";
 import NotificationPopover from "./notification-popover";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
@@ -8,6 +8,9 @@ import Link from "next/link";
 import WorkerProfileDialog from "./worker-profile-dialog";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Search from "./search";
+import { useState } from "react";
+import { Contact2, HardHat } from "lucide-react";
 
 const DashboardLayout = ({ children, role }) => {
   const { data: user } = useQuery(["user"], async () => {
@@ -16,13 +19,67 @@ const DashboardLayout = ({ children, role }) => {
     return response.data;
   });
 
+  const { data: users, isLoading } = useQuery(
+    ["users", "asd"],
+    async () => {
+      const response = await axios.get("/api/users");
+      return response.data;
+    },
+    {
+      onSuccess: (data) => {
+        setSearchResults(data);
+      },
+    }
+  );
+
+  const initialData = users;
+  const [searchResults, setSearchResults] = useState(initialData);
+
+  const handleSearch = (filteredData) => {
+    setSearchResults(filteredData);
+  };
+
+  useEffect(() => {
+    console.log("Search Results Updated:", searchResults);
+    // You can perform other actions based on the updated searchResults here
+  }, [searchResults]);
+
   return (
     <div className="max-w-7xl m-auto">
       {/* Header */}
       <div className="flex justify-between py-6 mb-6">
-        <h1 className="font-black text-emerald-500 tracking-tight text-xl uppercase">
-          Buildustry
-        </h1>
+        <div className="flex items-center gap-10">
+          <h1 className="font-black text-emerald-500 tracking-tight text-xl uppercase">
+            Buildustry
+          </h1>
+
+          {!isLoading && (
+            <div>
+              <Search data={initialData} onSearch={handleSearch} />
+              {searchResults && searchResults.length !== initialData.length && (
+                <ul className="absolute bg-white border border-gray-300 rounded-md p-2 shadow w-100 py-2 px-4 z-10 cursor-pointer">
+                  {searchResults.map((user) => (
+                    <WorkerProfileDialog worker={user} key={user.id}>
+                      <li className="py-2 px-3 border-b border-gray-300 ">
+                        <div className="flex-col flex">
+                          <p className="font-bold">{user.name}</p>
+                          <p className="flex gap-2">
+                            {user.role === "laborer" ? (
+                              <HardHat className="text-emerald-500 " />
+                            ) : (
+                              <Contact2 className="text-emerald-500  " />
+                            )}{" "}
+                            {user.role}
+                          </p>
+                        </div>
+                      </li>
+                    </WorkerProfileDialog>
+                  ))}
+                </ul>
+              )}
+            </div>
+          )}
+        </div>
         <div className="flex gap-4 items-center">
           <NotificationPopover />
 
